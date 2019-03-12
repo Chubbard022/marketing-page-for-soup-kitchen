36379910fc436311211a3434f91a268301cd213a
chubbard022 <chubbard02@gmail.com>
2019-03-12 10:23:23 -0700

    finished carousel

--- index.js
+++ index.js
@@ -4,7 +4,39 @@
 
 class Carousel{
     constructor(carousel){
-
+        this.carousel = carousel;
+        this.leftButton = document.querySelector('.left-button')
+        this.rightButton = document.querySelector('.right-button')
+        this.images = document.querySelectorAll('.carousel img')
+        //indexing pictures
+        this.index = 0;
+        this.currentImage = this.images[this.index];
+        this.currentImage.style.display = 'block'
+        //event listeners set to each button
+        this.leftButton.addEventListener('click',()=>this.previousImage())
+        this.rightButton.addEventListener('click',()=>this.nextImage())
+    }
+    previousImage(){
+        this.images.forEach((image)=>image.style.display = 'none')
+        this.index -= 1;
+        if(this.index < 0){
+            this.index = 3;
+        }
+        if(this.index > 3){
+            this.index = 0;
+        }
+        this.images[this.index].style.display = 'block';
+    }
+    nextImage(){
+        this.images.forEach((image)=>image.style.display = 'none')
+        this.index +=1;
+        if(this.index < 0){
+            this.index = 3;
+        }
+        if(this.index > 3){
+            this.index = 0;
+        }
+        this.images[this.index].style.display = 'block';
     }
 }
 
