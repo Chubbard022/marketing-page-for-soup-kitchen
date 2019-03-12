7dd8d083df07dff0ec4cd6285b8795b746e48943
chubbard022 <chubbard02@gmail.com>
2019-03-12 09:52:32 -0700

    started carousel in index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// const images = document.querySelectorAll('.carousel img');
+// const leftButton = document.querySelector('.left-button')
+// const rightButton = document.querySelector('.right-button')
+
+class Carousel{
+    constructor(carousel){
+
+    }
+}
+
+let carousel = document.querySelectorAll('.carousel').forEach((carousel)=> new Carousel(carousel))
